f011a6c5cccb59e1162737e36cda99d446fde057
TX-201705152306\Administrator <napoler2008@gmail.com>
2017-09-28 01:55:27 +0800

    实现基本的默认展示 js

--- kitchen-sink/pages/test.vue
+++ kitchen-sink/pages/test.vue
@@ -4,43 +4,41 @@
 
     <p>
       <a href="#" data-popup=".popup-login" class="open-popup">登录 </a>
+
+       <a href="#" data-popup=".popup-login" class="open-popup">登录 </a>
+
+       <p v-if="seen">Now you see me</p>
     </p>
-    
- 
 
-<!-- Login Screen -->
-<f7-login-screen class="popup-login" opened tablet-fullscreen>
-  <!-- Login Screen content goes here -->
+    <!-- Login Screen -->
+    <f7-login-screen class="popup-login" v-if="seen" opened>
+      <!-- Login Screen content goes here -->
 
- <f7-view>
-    <f7-pages>
-      <f7-page login-screen>
-        <f7-login-screen-title>登录</f7-login-screen-title>
-        <f7-list form>
-          <f7-list-item>
-            <f7-label>Username</f7-label>
-            <f7-input name="username" placeholder="Username"></f7-input>
-          </f7-list-item>
-          <f7-list-item>
-            <f7-label>Password</f7-label>
-            <f7-input name="password" type="password" placeholder="Password"></f7-input>
-          </f7-list-item>
-        </f7-list>
-        <f7-list>
-          <f7-list-button title="登录" close-login-screen></f7-list-button>
-          <f7-list-label>
-            <p>点击登录关闭</p> 
-          </f7-list-label>
-        </f7-list>
-      </f7-page>
-    </f7-pages>
-  </f7-view>
+      <f7-view>
+        <f7-pages>
+          <f7-page login-screen>
+            <f7-login-screen-title>登录</f7-login-screen-title>
+            <f7-list form>
+              <f7-list-item>
+                <f7-label>Username</f7-label>
+                <f7-input name="username" placeholder="Username"></f7-input>
+              </f7-list-item>
+              <f7-list-item>
+                <f7-label>Password</f7-label>
+                <f7-input name="password" type="password" placeholder="Password"></f7-input>
+              </f7-list-item>
+            </f7-list>
+            <f7-list>
+              <f7-list-button title="登录" close-login-screen></f7-list-button>
+              <f7-list-label>
+                <p>点击登录关闭</p>
+              </f7-list-label>
+            </f7-list>
+          </f7-page>
+        </f7-pages>
+      </f7-view>
 
-
-      
-
-
-</f7-login-screen>
+    </f7-login-screen>
   </f7-page>
 </template>
 <script>
@@ -48,13 +46,18 @@
 
 
 export default {
-   data: function () {
-      return {
-        login: 'flase'
-       
-      }
-      
-      }
+  data: function() {
+    return {
+      login: '',
+      seen:true,
+      opened:false
+
+    }
+
+  },
+  mounted(){
+  console.log('swipeout deleted');
+  }
 
 
 }
